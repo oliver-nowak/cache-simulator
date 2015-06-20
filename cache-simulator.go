5f4e9d80a387781b90de5d531ed9c5e9b82b8685
Oliver Nowak <oliver@olivernowak.com>
2015-06-20 11:57:18 -0400

    + added ability to display cache

--- cache-simulator.go
+++ cache-simulator.go
@@ -5,31 +5,55 @@ import (
 )
 
 // contiguous memory
-var main_memory = make([]byte, 2048)
+var main_memory = make([]uint16, 2048)
 
 // cache - 2d array composed of 16 blocks (16 bytes per block)
-var cache = make([][]byte, 16)
+var cache = make([][]uint16, 16)
+
+const (
+	BLOCK_OFFSET_MASK uint16 = 0x000F
+	SLOT_OFFSET_MASK  uint16 = 0x00F0 // >> 1
+	TAG_OFFSET_MASK   uint16 = 0xFF00 // >> 2
+	SLOT              uint16 = 0
+	VALID             uint16 = 1
+	TAG               uint16 = 2
+	DATA              uint16 = 3
+)
 
 func main() {
-	main_memory[0] = 14
-
-	fmt.Printf(">>> %d \n", main_memory[0])
-	fmt.Printf("+++ %d \n", main_memory[1])
-
-	// Zero cache, 16 bytes per block
+	// Initialize and Zero cache with 20 byte blocks
+	// 16 bytes / data block
+	// 1 byte / slot #
+	// 1 byte / valid flag
+	// 2 bytes / tag
 	for i := range cache {
-		block := make([]byte, 16)
+		block := make([]uint16, 20)
 		cache[i] = block
 	}
 
 	max := 256
 	mem_val := -1
 
-	// Initialize Main Memory
+	// Initialize Main Memory so that Address 0x100 = byte value 00, and so on.
 	for i := range main_memory {
 		mem_val = (mem_val + 1) % max
-		main_memory[i] = byte(mem_val)
+		main_memory[i] = uint16(mem_val)
 
 		fmt.Printf(">> %X \n", main_memory[i])
+	}
+
+	// assert 0x7FF = 0xFF
+	fmt.Printf("++ %X \n", main_memory[0x07FF])
+
+	displayCache()
+}
+
+func displayCache() {
+	fmt.Println("Slot  Valid   Tag    Data")
+
+	for entry := range cache {
+		row := cache[entry]
+
+		fmt.Printf("%2.1X     %2.1d     %2.1X     %X \n", row[SLOT], row[VALID], row[TAG], row[DATA:])
 	}
 }
