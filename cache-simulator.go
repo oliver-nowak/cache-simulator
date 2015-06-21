ae52655cb273506886019d8b63eaf9f55bb50a2b
Oliver Nowak <oliver@olivernowak.com>
2015-06-21 12:27:10 -0400

    + almost works

--- cache-simulator.go
+++ cache-simulator.go
@@ -7,21 +7,26 @@ import (
 )
 
 // contiguous memory
-var main_memory = make([]uint16, 2048)
+var main_memory = make([]byte, 2048)
 
 // cache - 2d array composed of 16 blocks (16 bytes per block)
-var cache = make([][]uint16, 16)
+var cache = make([][]byte, 16)
 
 var exit_requested = false
 
 const (
-	BLOCK_OFFSET_MASK uint16 = 0x000F
-	SLOT_OFFSET_MASK  uint16 = 0x00F0 // >> 1
-	// TAG_OFFSET_MASK   uint16 = 0xFF00 // >> 2
-	SLOT  uint16 = 0
-	VALID uint16 = 1
-	TAG   uint16 = 2
-	DATA  uint16 = 3
+	// BLOCK_OFFSET_MASK uint16 = 0x000F
+	// SLOT_OFFSET_MASK  uint16 = 0x00F0
+	SLOT    uint16 = 0
+	VALID   uint16 = 1
+	TAG     uint16 = 2
+	DATA    uint16 = 3
+	DATA_B2 uint16 = 5
+	DATA_B4 uint16 = 7
+	DATA_B6 uint16 = 9
+	DATA_B8 uint16 = 11
+	DATA_BA uint16 = 13
+	DATA_BC uint16 = 15
 )
 
 func main() {
@@ -31,7 +36,8 @@ func main() {
 	// 1 byte / valid flag
 	// 2 bytes / tag
 	for i := range cache {
-		block := make([]uint16, 20)
+		block := make([]byte, 20)
+		block[SLOT] = byte(i)
 		cache[i] = block
 	}
 
@@ -41,15 +47,8 @@ func main() {
 	// Initialize Main Memory so that Address 0x100 = byte value 00, and so on.
 	for i := range main_memory {
 		mem_val = (mem_val + 1) % max
-		main_memory[i] = uint16(mem_val)
-
-		// fmt.Printf(">> %X \n", main_memory[i])
+		main_memory[i] = byte(mem_val)
 	}
-
-	// assert 0x7FF = 0xFF
-	// fmt.Printf("++ %X \n", main_memory[0x07FF])
-
-	// displayCache()
 
 	// User Input loop
 	for !exit_requested {
@@ -65,14 +64,14 @@ func displayMenu() {
 }
 
 func displayCache() {
-	fmt.Println("==========================================================================")
-	fmt.Println("================================C A C H E ================================")
+	fmt.Println("====================================================================================")
+	fmt.Println("================================ C A C H E =========================================")
 	fmt.Println("Slot  Valid   Tag    Data")
 
 	for entry := range cache {
 		row := cache[entry]
 
-		fmt.Printf("%2.1X     %2.1d     %2.1X     %2.2X \n", row[SLOT], row[VALID], row[TAG], row[DATA:])
+		fmt.Printf("%2.1X     %2.1d     %2.1X     %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X  %2.2X\n", row[SLOT], row[VALID], row[TAG], row[3:4], row[4:5], row[5:6], row[6:7], row[7:8], row[8:9], row[9:10], row[10:11], row[11:12], row[12:13], row[13:14], row[14:15], row[15:16], row[16:17], row[17:18], row[18:19])
 	}
 }
 
@@ -91,20 +90,33 @@ func readData() {
 	fmt.Printf("Reading Main_Memory[%d] \n", idx)
 
 	// get the data at that location
-	var mem uint16 = main_memory[idx]
+	var mem byte = main_memory[idx]
 	fmt.Printf("Data: [%4.4X] \n", mem)
 
-	var block uint16 = mem & BLOCK_OFFSET_MASK
-	var slot uint16 = (mem & SLOT_OFFSET_MASK) >> 4
-	var tag uint16 = mem >> 8
+	var block byte = mem & 0x0F
+	var slot byte = (mem & 0xF0) >> 4
+	var tag byte = mem >> 8
 
 	fmt.Printf("Tag: [%2.2X]    Slot: [%X]   Block: [%X] \n", tag, slot, block)
+	fmt.Printf("At that byte there is the value %2.2X \n", mem)
 
 	// check if its in the cache
+	// TODO: implement cache check
 
 	// read the entire data block if its not in the cache
+	var addr_begin = idx - int64(block)
+	var addr_end = addr_begin + 16
+	var data_block = make([]byte, 16)
+	data_block = main_memory[addr_begin:addr_end]
 
-	fmt.Printf("At that byte there is the value %2.2X \n", mem)
+	fmt.Printf("Data retrieved: %X \n", data_block[:])
+
+	// write it to the cache
+	var slot_idx = int(slot)
+	var cache_row = cache[slot_idx]
+	cache_row[VALID] = 0x01
+	cache_row[TAG] = tag
+	copy(cache_row[DATA:], data_block[:])
 }
 
 func getMenuInput() {
